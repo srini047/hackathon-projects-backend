a2cae121c1a32add4232c4e5d046ea9d55a24c0b
srini047 <srinikethcr7@gmail.com>
2023-02-19 01:14:47 +0530

    fix: vercel deployment

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ const port = 3000;
 
 app.use(express.json());
 
+app.get("/", function (req, res) {
+  res.json({ "message": "SUCCESS: 200 OK" });
+});
 app.use("/projectIdeas", projectIdeas);
 app.use("/projectIdea", projectIdea);
 app.use("/projectDescription", projectDescription);
 app.use("/projectTitle", projectTitle);
 app.use("/projectImplement", projectImplement);
-
 
 app.listen(port, () => console.log(`App running on port ${port}`));
